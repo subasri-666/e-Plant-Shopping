678263db9b85d2d2044c13b383849da55fbc6bc0
subasri s <smsubasri555@gmail.com>
2026-06-05 18:26:58 +0530

    Create AboutUs.jsx

---
+++ AboutUs.jsx
@@ -0,0 +1,26 @@
+import React from "react";
+
+function AboutUs() {
+  return (
+    <div className="about-container">
+      <h2>About e-Plant Shopping</h2>
+
+      <p>
+        Welcome to e-Plant Shopping, your one-stop destination for
+        beautiful indoor and outdoor plants.
+      </p>
+
+      <p>
+        We provide a wide range of plants that help create a greener,
+        healthier, and more sustainable environment.
+      </p>
+
+      <p>
+        Browse our collection, add your favorite plants to the cart,
+        and bring nature closer to your home.
+      </p>
+    </div>
+  );
+}
+
+export default AboutUs;
